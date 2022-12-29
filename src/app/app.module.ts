8fdb29d14ccb0ee165b384d43d2a19fdf8161923
Jared Blecher <jared.blecher@idc.post.ac.il>
2022-12-29 12:02:55 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddItemComponent } from './home/add-item/add-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddItemComponent
   ],
   imports: [
     BrowserModule
